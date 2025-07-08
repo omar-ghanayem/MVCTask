41d41bb9c1d10aeca13bcd354a9f654f75e95c41
omar-ghanayem <omarghanayem986@gmail.com>
2025-07-08 23:52:08 +0300

    MvcFirstTask

---
+++ WebApplication1/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        ApplicationDbContext context= new ApplicationDbContext();
+        public ViewResult Index()
+        {
+            var products= context.Products.ToList();
+            return View(products);
+        }
+        public ViewResult Create()
+        {
+            return View();
+        }
+
+        public RedirectToActionResult Add(Product requst)
+        {
+            context.Products.Add(requst);
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        public ViewResult Details(int id)
+        {
+            var products= context.Products.Find(id);
+            return View(products);
+        }
+    }
+}
---
+++ WebApplication1/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=.;Database=MvcTask;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+    }
+}
